3898fdfe602e6e7d8853dfd78f42501b248c4e22
Björn Möller Ehrnlund <Bjorn@Bjorns-MBP.lan>
2018-08-13 22:56:53 +0200

    implemented auto update

--- app/transmitter.js
+++ app/transmitter.js
@@ -4,31 +4,50 @@ const Gpio = require ('onoff').Gpio;
 
 const devicePins = [new Gpio(4, 'out')];
 
-const updateNode = async (nodeId) => {
+const updateNodeById = async (nodeId) => {
   Node.find(nodeId, function(node) {
-    if (node.control) {
-      devicePins[node.pin].writeSync(node.controlPower ? 1 : 0);
-    } else {
-      Interval.findAll(function(intervals) {
-        nodeIntervals = intervals.filter(i => i.nodeId == nodeId);
-        var time = getTime();
-        var active = false;
-
-        nodeIntervals.map(i => {
-          if (i.start < i.end){
-            if (time >= i.start && time < i.end)
-              active = true;
-
-          } else {
-            if (time >= i.start || time >= i.end)
-              active = true;
-          }
-        });
-        devicePins[node.pin].writeSync(active ? 1 : 0);
-      })
-    }
+    updateNode(node);
   });
 }
+
+function updateNode(node) {
+  if (node.control){
+    normalUpdate(node);
+  } else {
+    intervalUpdate(node);
+  }
+}
+
+function normalUpdate(node) {
+  devicePins[node.pin].writeSync(node.controlPower ? 1 : 0);
+}
+
+function intervalUpdate(node){
+  Interval.findAll(function(intervals) {
+    nodeIntervals = intervals.filter(i => i.nodeId == nodeId);
+    var time = getTime();
+    var active = false;
+
+    nodeIntervals.map(i => {
+      if (i.start < i.end){
+        if (time >= i.start && time < i.end)
+          active = true;
+
+      } else {
+        if (time >= i.start || time >= i.end)
+          active = true;
+      }
+    });
+    devicePins[node.pin].writeSync(active ? 1 : 0);
+  })
+}
+
+
+setInterval(function(){
+  Node.findAll(function (nodes){
+    nodes.map(n => {updateNode(n)});
+  });
+}, 60000)
 
 function getTime() {
   let d = new Date();
